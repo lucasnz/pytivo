438825b55e4913c6663134b0f6525f30becff970
lucas_nz <lucas_nz@hotmail.com>
2016-02-20 14:35:40 +1300

    temp share support

--- plugins/video/video.py
+++ plugins/video/video.py
@@ -68,6 +68,13 @@ class Pushable(object):
         file_info = VideoDetails()
         file_info['valid'] = transcode.supported_format(f['path'])
 
+        temp_share = config.get_server('temp_share', '')
+        temp_share_path = ''
+        if temp_share:
+            for name, data in config.getShares():
+                if temp_share == name:
+                    temp_share_path = data.get('path')
+
         mime = 'video/mpeg'
         if config.isHDtivo(f['tsn']):
             for m in ['video/mp4', 'video/bif']:
@@ -77,10 +84,15 @@ class Pushable(object):
 
             if (mime == 'video/mpeg' and
                 transcode.mp4_remuxable(f['path'], f['tsn'])):
-                new_path = transcode.mp4_remux(f['path'], f['name'], f['tsn'])
+                new_path = transcode.mp4_remux(f['path'], f['name'], f['tsn'], temp_share_path)
                 if new_path:
                     mime = 'video/mp4'
                     f['name'] = new_path
+                    if temp_share_path:
+                        ip = config.get_ip()
+                        port = config.getPort()
+                        container = quote(temp_share) + '/'
+                        f['url'] = 'http://%s:%s/%s' % (ip, port, container)
 
         if file_info['valid']:
             file_info.update(self.metadata_full(f['path'], f['tsn'], mime))
